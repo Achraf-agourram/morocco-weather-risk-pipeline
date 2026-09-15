aed6ee8ea62f336595095c929f44dc0408ce41aa
Achraf-agourram <achrafagourram6@gmail.com>
2026-09-15 12:07:01 +0100

    feat: added quality checks functions

--- src/transformation.py
+++ src/transformation.py
@@ -77,3 +77,16 @@ def remove_duplicates(df):
         ]
     )
 
+def quality_checks(df):
+    results = {
+        "valid_dates": df["date"].notna().all(),
+        "valid_latitudes": df["latitude"].notna().all(),
+        "valid_longitudes": df["longitude"].notna().all(),
+        "valid_temperatures": (df["temperature_min"] <= df["temperature_max"]).all(),
+        "valid_precipitation": (df["precipitation"] >= 0).all(),
+        "valid_rain_probability": ((df["rain_probability"] >= 0) & (df["rain_probability"] <= 100)).all(),
+        "valid_wind_speed": (df["wind_speed_max"] >= 0).all()
+    }
+
+    return results
+
