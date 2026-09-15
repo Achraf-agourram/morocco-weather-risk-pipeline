f6beb3ddd7491eb063ca96eb0f7a67479dae96b8
Achraf-agourram <achrafagourram6@gmail.com>
2026-09-15 16:52:05 +0100

    feat: categorizing max temperatures into interval

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from src.loading import *
 
 # save_silver_data(SILVER_FILE, join_cities(BRONZE_CITIES, remove_duplicates(handle_missing_values(standardize_types(transform_weather(load_weather(BRONZE_WEATHER)))))))
 
-print(load_silver_data(SILVER_FILE))
+print(add_temperature_category(load_silver_data(SILVER_FILE)))
--- src/transformation/features.py
+++ src/transformation/features.py
@@ -7,3 +7,14 @@ GOLD_FILE = "data/gold/weather_features.csv"
 
 def load_silver_data(file):
     return pd.read_csv(file)
+
+def add_temperature_category(df):
+
+    df["temperature_category"] = pd.cut(
+        df["temperature_max"],
+        bins=[-float("inf"), 5, 15, 25, 35, float("inf")],
+        labels=["very_cold", "cold", "mild", "warm", "hot"],
+        right=False
+    )
+
+    return df
