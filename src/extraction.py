0b5b9e7d564e7716b45a8de19ab43fda51e5a991
Achraf-agourram <achrafagourram6@gmail.com>
2026-09-18 14:58:54 +0100

    fix: extract cities and weathers using one request

--- src/extraction.py
+++ src/extraction.py
@@ -1,41 +1,47 @@
-def extract_cities ():
+import requests as re
+import json
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+load_dotenv()
+
+BRONZE_WEATHER = os.getenv("BRONZE_WEATHER")
+BRONZE_CITIES = os.getenv("BRONZE_CITIES")
+WEATHER_URL = os.getenv("WEATHER_URL")
+CITIES_URL = os.getenv("CITIES_URL")
+
+def extract_cities (url=CITIES_URL, file=BRONZE_CITIES):
+
+    response = re.get(url)
+
+    with open(file, "wb") as file:
+        file.write(response.content)
+
     return True
 
-def extract_weather ():
+def extract_weather (cities=pd.read_csv(BRONZE_CITIES), url=WEATHER_URL, file=BRONZE_WEATHER):
+
+    params = {
+        "latitude": ",".join(cities["latitude"].astype(str)),
+        "longitude": ",".join(cities["longitude"].astype(str)),
+        "daily": [
+            "temperature_2m_max",
+            "temperature_2m_min",
+            "precipitation_sum",
+            "precipitation_probability_max",
+            "wind_speed_10m_max",
+            "wind_gusts_10m_max",
+            "weather_code"
+        ],
+        "forecast_days": 7,
+        "timezone": "Africa/Casablanca"
+    }
+
+    response = re.get(url, params=params)
+    data = response.json()
+
+    with open(file, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
+
     return True
-
-
-
-
-
-
-
-
-
-
-
-
-# url = "https://api.open-meteo.com/v1/forecast"
-
-# params = {
-#     "latitude": 32.2833,
-#     "longitude": -9.2333,
-#     "daily": [
-#         "temperature_2m_max",
-#         "temperature_2m_min",
-#         "precipitation_sum",
-#         "precipitation_probability_max",
-#         "wind_speed_10m_max",
-#         "wind_gusts_10m_max",
-#         "weather_code"
-#     ],
-#     "forecast_days": 7,
-#     "timezone": "Africa/Casablanca"
-# }
-
-# response = re.get(url, params=params, timeout=10)
-
-# data = response.json()
-
-# with open("src/bronze/weather.json", "w", encoding="utf-8") as file:
-#     json.dump(data, file, indent=4, ensure_ascii=False)
